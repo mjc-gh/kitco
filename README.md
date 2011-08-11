48b480af4561b3f56d4495583ff331c36a15eacd
mikeycgto <mikeycgto@gmail.com>
2011-08-10 22:04:35 -0400

    first commit

---
+++ README.md
@@ -0,0 +1,27 @@
+Kitco Gem
+=========
+
+This gem retrieves data (via HTTParty) from Kitco Charts about Gold and other precious metals.
+
+Use at your own discretion 
+
+Install
+--------
+	gem install kitco
+
+
+Examples
+--------
+	require 'kitco'
+
+	puts Kitco.gold
+	puts Kitco.silver
+
+Command Line Utility
+--------------------
+
+    kitco gold
+
+For more details on run
+
+    kitco --help
---
+++ lib/kitco.rb
@@ -0,0 +1,44 @@
+require 'httparty'
+require 'base64'
+require 'time'
+
+class Kitco
+  include HTTParty
+
+  base_uri "charts.kitco.com/KitcoCharts"
+  parser(lambda { |body, format|
+    # they don't send an XML content-type if its a bad request
+    return Crack::XML.parse(body) if body.include?('xml')
+  
+    results = {}
+    data = Base64.decode64(body).split(',')
+
+    results[:time] = Time.parse data.shift
+    results[:percentage] = data.pop.to_f / 100.0
+    
+    data.collect! { |n| n.to_f }
+    results.merge! Hash[ [:bid, :ask, :change, :low, :high].zip(data) ]
+  })
+  
+  def self.gold 
+    request :gold
+  end
+  
+  def self.silver
+    request :silver
+  end
+  
+  def self.palladium
+    request :palladium
+  end 
+  
+  def self.rhodium
+    request :rhodium
+  end
+  
+  protected
+  
+  def self.request symbol
+    get '/RequestHandler', :query => {:requestName => 'getSymbolSnapshot', :Symbol => symbol.upcase }
+  end
+end
