d7d56b967c76f727f5590ed328b6b0fcd389450a
mikeycgto <mikeycgto@gmail.com>
2014-01-07 14:51:53 -0500

    Use to_s on symbol before calling upcase

--- lib/kitco.rb
+++ lib/kitco.rb
@@ -9,38 +9,38 @@ class Kitco
   parser lambda { |body, format|
     # they don't send an XML content-type if its a bad request
     return Crack::XML.parse(body) if body.include?('xml')
-  
+
     results = {}
     data = Base64.decode64(body).split(',')
 
     results[:time] = Time.parse data.shift
     results[:percentage] = data.pop.to_f / 100.0
-    
+
     data.collect! { |n| n.to_f }
     results.merge! Hash[ [:bid, :ask, :change, :low, :high].zip(data) ]
   }
-  
-  def self.gold 
+
+  def self.gold
     request :gold
   end
-  
+
   def self.silver
     request :silver
   end
-  
+
   def self.palladium
     request :palladium
-  end 
-  
+  end
+
   def self.rhodium
     request :rhodium
   end
-  
+
   class << self
     def request symbol
-      get '/RequestHandler', :query => {:requestName => 'getSymbolSnapshot', :Symbol => symbol.upcase }
+      get '/RequestHandler', :query => {:requestName => 'getSymbolSnapshot', :Symbol => symbol.to_s.upcase }
     end
-    
+
     protected :request
   end
 end
